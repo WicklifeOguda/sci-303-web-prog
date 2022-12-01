e5e3b2ee50f4c872ec63269073383d6c81ca9c3d
Wicklife Oguda <ogudawicklife1@gmail.com>
2022-12-01 18:41:15 +0300

    Prevent scrolling past footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,20 @@
 const year = new Date()
 const copy = document.getElementById("copy")
 copy.innerHTML = year.getFullYear()
+
+// seting fixed scroll 
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
+window.addEventListener("scroll", () => {
+    const footerYPos = Math.ceil(getOffset("footer").top) 
+    const scrolled = Math.ceil(window.scrollY)
+    if (scrolled > footerYPos) {
+       window.scrollTo({top:footerYPos, left: 0, behavior: 'smooth'})
+    }
+
+})
